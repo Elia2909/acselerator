864421e6a089f36336a742a25e7665374cef2b4a
Elia2909 <bulkaus@gmail.com>
2020-04-27 00:26:06 +0300

    Adapt

--- path.js
+++ path.js
@@ -13,14 +13,47 @@ label.addEventListener('click', (event) =>{
 
 
 $('.result__slider').slick({
-    infinite: true,
     slidesToShow: 2,
     slidesToScroll: 1,
-    arrows:true
+    arrows:true,
+    responsive:[
+        {
+            breakpoint: 1276,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+        }
+    }
+    ]
   });
 
   $('.partnes__firms-slider').slick({
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive:[
+        {
+            breakpoint:1100,
+            settings:{
+                slidesToScroll1: 1,
+                slidesToShow: 2
+            }
+        },
+    {
+      breakpoint: 830,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    ]
   });
+  $(document).ready(function(){
+      var menuBtn = $('.header-down button');
+      var menu = $('.burger');
+
+      menuBtn.on('click' ,function(event){
+        event.preventDefault();
+        menu.toggleClass('active')
+      })
+  })
